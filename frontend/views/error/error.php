5f3e19cbfe977e21034f5e9cc0d8fab179b9e491
1 <1>
2018-01-08 14:59:09 +0800

    错误信息页面1

--- frontend/views/error/error.php
+++ frontend/views/error/error.php
@@ -7,4 +7,12 @@ $this->title = $name;
 $this->context->layout = false; //不使用布局,或者改为自己所需要使用的布局
 
 ?>
-错误
+<div class="site-error">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <div class="alert alert-danger">
+        <?= nl2br(Html::encode($message)) ?>
+    </div>
+
+</div>
